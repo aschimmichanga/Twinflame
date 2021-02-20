afe83b56a8bdaef474f1b9175ae41a111994f046
richardwzp <wzprichardwzp@gmail.com>
2021-02-19 23:54:36 -0500

    convert given submission into JSON format

--- test.js
+++ test.js
@@ -1,8 +1,39 @@
+// if answers are not fully filled, alert, or else record
 function check() {
-    var c = 0;
-    var q1 = document.quiz.question1;
-    console.log(q1);
-    if (q1 == "A. test1") 
-    {c++}
-    document.write(c);
+    var reqLength = 6;
+    allArrayElements = Array.from(document.querySelector("#quiz_id").elements);
+    first = document.getElementById("fname").value;
+    last = document.getElementById("lname").value; 
+    var len = allArrayElements.filter(element => element.checked).length;
+
+    if (len == reqLength && first != null && last != null)
+    {
+    console.log(record(allArrayElements, [first, last]));
+}
+    
+    else
+    alert("fill out all the questions!");
+}
+// record the answers
+function record(ar, names) {
+ 
+    var q1; 
+    var answer = [];
+
+    answer.push(names[0]);
+     answer.push(names[1]);
+    ar.forEach(element => {
+        if (element.type == "radio" && element.checked)
+        {answer.push(element.value);
+        //console.log(element);
+        }
+    });
+    return arrayToJson(answer);
+    console.log(answer);
+}
+
+function arrayToJson(ar) {
+    return JSON.stringify({firstName: ar[0],
+            lastName: ar[1],
+            answers: ar.splice(2, ar.length)});
 }
